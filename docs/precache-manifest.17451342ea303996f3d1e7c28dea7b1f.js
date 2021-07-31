03cb534887b98c71f8705da9116e2c72bbcfdf0e
Paul <pavelproggrammer@gmail.com>
2021-07-31 22:41:41 +0300

    my work

---
+++ docs/precache-manifest.17451342ea303996f3d1e7c28dea7b1f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3b2fe1557946a3d1bb5e7a48a952f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1913af74dfbc65357fd",
+    "url": "/static/css/2.a5fa21f8.chunk.css"
+  },
+  {
+    "revision": "50438077197ed809c967",
+    "url": "/static/css/main.e5b4d591.chunk.css"
+  },
+  {
+    "revision": "b1913af74dfbc65357fd",
+    "url": "/static/js/2.ce6833f9.chunk.js"
+  },
+  {
+    "revision": "a9ac79dc5019bbd8d0de0c87e1e52273",
+    "url": "/static/js/2.ce6833f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50438077197ed809c967",
+    "url": "/static/js/main.f699dff5.chunk.js"
+  },
+  {
+    "revision": "4aed2107549889e221b9",
+    "url": "/static/js/runtime-main.f67154a1.js"
+  },
+  {
+    "revision": "6a2e3b7302e45c40495c763c069ff2f6",
+    "url": "/static/media/Vector.6a2e3b73.svg"
+  },
+  {
+    "revision": "1c998da6466c5ce0a3155f9862de10a1",
+    "url": "/static/media/search.1c998da6.svg"
+  }
+]);
